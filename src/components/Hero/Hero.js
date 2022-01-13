b4c7e07b80e0ce36d974ba5738a246543d123e68
nscando <nicolas.scandizzo@gmail.com>
2022-01-13 15:16:45 -0300

    editing portfolio

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row noppading>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I'm Nicolás, <br />
+        I’m a web & soft­ware devel­op­er from Argentina
+      </SectionTitle>
+      <SectionText>
+        I have been working as a freelance web designer and software developer since 2013.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { DiFirebase, DiReact, DiJava } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range a technologies in the web development world.
+      From Back-end To Design.
+    </SectionText>
+    <List>
+      <ListItem>
+        <picture>
+          <DiReact size="4rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experiece with <br />
+            React.js <br />
+            Angular
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiFirebase size="4rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node and Databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiJava size="4rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Others</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Java<br />
+            Kotlin <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
+);
+
+export default Technologies;
